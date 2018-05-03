e968e872554f9b6e8469182f12408ae10e0c431a
Patrick Schuelke <p-schuelke@gmx.de>
2018-05-03 10:19:55 +0200

    add placeholder components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import Home from './Home';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Public from './Public';
 
 class App extends Component {
   state = {};
 
   render() {
-    return <p>Its working</p>;
+    return (
+      <div className="container">
+        <h1>React Auth Exercise</h1>
+        <Home />
+        <Login />
+        <Public />
+        <Dashboard />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Login extends Component {
+  render() {
+    return <div>Login Page</div>;
+  }
+}
