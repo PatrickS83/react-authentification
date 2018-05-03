734af942a6ac83447dce775be3b21848515651a8
Patrick Schuelke <p-schuelke@gmx.de>
2018-05-03 10:37:47 +0200

    add buttons to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const Home = () => <div>This is the home</div>;
+const Home = () => (
+  <div>
+    <h1>Welcome User!</h1>
+    <p>Where do you want to go?</p>
+    <div className="row">
+      <button className="button-primary four columns">Public Page</button>
+      <button className="button-primary four columns">
+        Protected Dashboard
+      </button>
+    </div>
+  </div>
+);
 
 export default Home;
