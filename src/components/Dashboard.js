80c0215632a4cfcefd70378e411a9172301ffb9f
Patrick Schuelke <p-schuelke@gmx.de>
2018-05-03 10:40:02 +0200

    added informational text to dashboard and public.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Dashboard = () => <div>Dashboard</div>;
+const Dashboard = () => (
+  <div>
+    <h1>This is the protected admin page</h1>
+    <p>It is accessible only if you are logged in!</p>
+  </div>
+);
 
 export default Dashboard;
--- src/components/Public.js
+++ src/components/Public.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Public = () => <div>Public</div>;
+const Public = () => (
+  <div>
+    <h1>This is the public page</h1>
+    <p>It is accessible by everyone</p>
+  </div>
+);
 
 export default Public;
