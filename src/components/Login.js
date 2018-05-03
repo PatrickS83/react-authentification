b24f4733c12f713ad2d80d51421fd50da6232b95
Patrick Schuelke <p-schuelke@gmx.de>
2018-05-03 10:33:10 +0200

    add login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,29 @@ import PropTypes from 'prop-types';
 
 export default class Login extends Component {
   render() {
-    return <div>Login Page</div>;
+    return (
+      <form>
+        <h1>Account Login</h1>
+        <div className="twelve columns">
+          <label htmlFor="username">Your Username</label>
+          <input
+            className="u-full-width"
+            type="email"
+            placeholder="Username"
+            id="username"
+          />
+        </div>
+        <div className="twelve columns">
+          <label htmlFor="email">Your email</label>
+          <input
+            className="u-full-width"
+            type="email"
+            placeholder="test@mailbox.com"
+            id="email"
+          />
+        </div>
+        <input className="button-primary" type="submit" value="Submit" />
+      </form>
+    );
   }
 }
