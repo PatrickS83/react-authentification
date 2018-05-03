9879806a6632459ed4d1a082cf137f7cb9b08550
Patrick Schuelke <p-schuelke@gmx.de>
2018-05-03 11:50:28 +0200

    minor webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
   },
   module: {
-
     rules: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
+        loader: 'babel-loader'
+      }
     ]
-  },
+  }
 };
-
